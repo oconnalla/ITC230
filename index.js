41b1c5f267d153bb146745c126010baaea73671d
oconnalla <oconnalla@gmail.com>
2019-06-25 18:40:30 -0700

    practicing with commits

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ http.createServer((req,res) => {
     break;
   case '/about':
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('About page');
+    res.end('About Page');
     break;
   default:
     res.writeHead(404, {'Content-Type': 'text/plain'});
