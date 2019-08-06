9d47640a9df02946f8233a72a993a7f22adc3e45
oconnalla <oconnalla@gmail.com>
2019-08-05 23:47:00 -0700

    attempted to update api methods to newest class exmaples, but the methods are not being recognized

--- index.js
+++ index.js
@@ -82,18 +82,23 @@
 
 
 //////////////////////////////////////////////////////////////////
-//HOMEWORK 3 EXPRESS YOURSELF VERSION//
+//HOMEWORK APIS//
 //////////////////////////////////////////////////////////////////
 
 'use strict';
 // bringing in my file containing my methods
 const data = require('./modules/data');
+//To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
+const bodyParser = require('body-parser');
+
+
+
 //bringing in express to app
 const express = require('express');
 //assigning express method to app variable
 const app = express();
-//To handle HTTP POST request in Express.js version 4 and above, you need to install middleware module called body-parser.
-const bodyParser = require('body-parser');
+
+//CONFIGURE EXPRESSS
 app.set('port', process.env.PORT || 3000);
 // set location for static files
 //__dirname tells the path (for ex:docs\ITC230)
@@ -102,6 +107,11 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({extended: true}));
 // set Access-Control-Allow-Origin header for api route
 app.use('/api', require('cors')());
+app.use((err, req, res, next) => {
+  console.log(err);
+});
+
+
 //lets this file access handlebars
 const handlebars =  require('express-handlebars');
 //tells express what template to use and the file extension
@@ -170,6 +180,7 @@ app.post('/detail', (req,res) => {
 
 ////////////////////////////////////////////////////////////////////////API SECTION//
 //////////////////////////////////////////////////////////////////////
+//EXAMPLE:
 // app.get('/api:char'),(req, res, next) =>{
 //   data.find((err, item) => {
 //     if(err || !item) return next(err);
@@ -178,55 +189,118 @@ app.post('/detail', (req,res) => {
 // };
 
 
-app.get('/api/char', (req,res) => {
-  // return all items in badDndChar array
-  var char = data.getAll();
-  if (char) {
-    // turn it into JSON 
-    res.json(char);
-    //else return an error
-  } else {
-    return res.status(500).send('Error occurred: database error.');
-  }
+
+
+
+//ANOTHER WAY TO DO THE GETALL METHOD
+// app.get('/api/char', (req,res) => {
+//   // return all items in badDndChar array
+//   var char = data.getAll();
+//   if (char) {
+//     // turn it into JSON 
+//     res.json(char);
+//     //else return an error
+//   } else {
+//     return res.status(500).send('Error occurred: database error.');
+//   }
+// });
+
+app.get('/api/char', (req,res, next) => {
+  data.find((err,results) => {
+      if (err || !results) return next(err);
+      res.json(results);
+  });
 });
 
 
-app.get('/api/:name', (req,res) => {
-  let char = data.get(req.params.name)
-  if (char) {
-    // turn it into JSON 
-    res.json(char);
-    //else return an error
-  } else {
-    return res.status(500).send('Error occurred: database error.');
+
+//ANOTHER WAY TO DO THE GET METHOD
+// app.get('/api/:name', (req,res) => {
+//   let char = data.get(req.params.name)
+//   if (char) {
+//     // turn it into JSON 
+//     res.json(char);
+//     //else return an error
+//   } else {
+//     return res.status(500).send('Error occurred: database error.');
+//   }
+// });
+
+app.get('/api/:name', (req, res, next) => {
+  let name = req.params.name;
+  data.findOne({name: name}, (err, result) => {
+      if (err || !result) return next(err);
+      res.json( result );    
+  });
+});
+
+
+
+
+//ANOTHER VERSION OF THE DELETE METHOD
+// app.get('/api/delete/:name', (req,res) => {
+//   let char = data.deleted(req.params.name);
+//   console.log(char);
+//   let total = data.getAll().length;
+//   if (char) {
+//     // turn it into JSON 
+//     res.json('Your request to remove ' + req.params.name + ' was successful. Remaining characters: ' + total);
+//     //else return an error
+//   } else {
+//     return res.status(500).send('Error occurred: database error. Cannot delete character');
+//   }
+// });
+
+app.get('/api/deleted:name', (req,res, next) => {
+  data.remove({'name':req.params.name }, (err, result) => {
+      if (err) return next(err);
+      // return # of items deleted
+      res.json({'deleted': result.result.n});
+  });
+});
+
+//ANOTHER VERSION OF THE ADDED METHOD
+// app.post('/api/add/:name:strenth:weakness', (req,res) => {
+//   let char = data.added(req.params);
+//   let total = data.getAll().length;
+//   if (char) {
+//     // turn it into JSON 
+//     res.json('Your request to add ' + req.params.name + ' was successful. Remaining characters: ' + total);
+//     //else return an error
+//   } else {
+//     return res.status(500).send('Error occurred: database error. Cannot add character');
+//   }
+// });
+
+// app.get('/api/add/:name/:strength/:weakness', (req,res, next) => {
+//   // find & update existing item, or add new 
+//   let name = req.params.name;
+//   data.update({ name: name}, {name:name, strength: req.params.strength, weakness: req.params.weakness }, {upsert: true }, (err, result) => {
+//       if (err) return next(err);
+//       // nModified = 0 for new item, = 1+ for updated item 
+//       res.json({updated: result.nModified});
+//   });
+// });
+
+
+app.post('/api/added/', (req,res, next) => {
+  // find & update existing item, or add new 
+   // insert new document
+  if (!req.body._id) {
+      let Char = new Char({name:req.body.name,strength:req.body.strength,weakness:req.body.weakness});
+      char.save((err,newChar) => {
+          if (err) return next(err);
+          res.json({updated: 0, _id: newChar._id});
+      });
+      // update existing document
+  } else { 
+      Char.updateOne({ _id: req.body._id}, {name:req.body.name, strength: req.body.strength, weakness: req.body.weakness }, (err, result) => {
+          if (err) return next(err);
+          res.json({updated: result.nModified, _id: req.body._id});
+      });
   }
 });
 
-app.get('/api/delete/:name', (req,res) => {
-  let char = data.deleted(req.params.name);
-  console.log(char);
-  let total = data.getAll().length;
-  if (char) {
-    // turn it into JSON 
-    res.json('Your request to remove ' + req.params.name + ' was successful. Remaining characters: ' + total);
-    //else return an error
-  } else {
-    return res.status(500).send('Error occurred: database error. Cannont delete character');
-  }
-});
-
-app.put('/api/add/:name:strenth:weakness', (req,res) => {
-  let char = data.added(req.params);
-  console.log(char);
-  let total = data.getAll().length;
-  if (char) {
-    // turn it into JSON 
-    res.json('Your request to add ' + req.params.name + ' was successful. Remaining characters: ' + total);
-    //else return an error
-  } else {
-    return res.status(500).send('Error occurred: database error. Cannont delete character');
-  }
-});
 
 // define 404 handler in case route doesn't work
 app.use( (req,res) => {
